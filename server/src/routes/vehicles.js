8c49e688e5b4187447c9b5dcf429f8769adac1c7
Juan Esteban Londoño Uribe <jelu0204@gmail.com>
2024-11-01 19:31:43 -0500

    actualizacion de rutas

--- server/src/routes/vehicles.js
+++ server/src/routes/vehicles.js
@@ -1,17 +1,22 @@
-const {Router} = require("express")
-const {getVehicles, addVehicle, exitVehicle, getAutosPlazas, getMotosPlazas, getPesadosPlazas} = require('../controllers/parking.controller.js')
-const router = Router()
+const { Router } = require('express');
+const {
+    getVehicles,
+    addVehicle,
+    exitVehicle,
+    getAutosPlazas,
+    getMotosPlazas,
+    getPesadosPlazas,
+} = require('../controllers/parking.controller.js');
+const router = Router();
 
-router.route("/")
-    .get(getVehicles)
-    
-router.route("/:id")
-    .delete(exitVehicle)
-    
-router.route('/parking')
-    .post(addVehicle)
-    .get(getAutosPlazas)
-    .get(getMotosPlazas)
-    .get(getPesadosPlazas)
+router.route('/').get(getVehicles);
+
+router.route('/:id').delete(exitVehicle);
+
+router.route('/parking').post(addVehicle);
+
+router.route('/parking/autos').get(getAutosPlazas);
+router.route('/parking/motos').get(getMotosPlazas);
+router.route('/parking/pesados').get(getPesadosPlazas);
 
 module.exports = router;
